f8b3adcf936d414157276825fc24191e2bd394a0
MathewsGuz <matguz96@hotmail.com>
2017-09-28 13:00:41 -0500

    vista registrar

--- src/main/java/edu/eci/pdsw/samples/managebeans/RegistroConsultaBean.java
+++ src/main/java/edu/eci/pdsw/samples/managebeans/RegistroConsultaBean.java
@@ -6,10 +6,15 @@
 package edu.eci.pdsw.samples.managebeans;
 
 
+import com.google.common.base.Strings;
+import edu.eci.pdsw.samples.entities.Eps;
+import edu.eci.pdsw.samples.services.ExcepcionServiciosPacientes;
 import edu.eci.pdsw.samples.services.ServiciosHistorialPacientesFactory;
 import edu.eci.pdsw.samples.services.ServiciosPacientes;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 import javax.faces.application.FacesMessage;
 
@@ -25,6 +30,14 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean(name = "HistorialPacientes")
 @SessionScoped
 public class RegistroConsultaBean implements Serializable {
+    private String nombre;
+    private int id;
+    private String tipoId;
+    private Date fechaNacimiento;
+    private Eps eps;
+    private List<String> nombreEps;
+
+
 
     private final ServiciosPacientes servicepacientes = ServiciosHistorialPacientesFactory.getInstance().getServiciosPaciente();
 
@@ -37,6 +50,54 @@ public class RegistroConsultaBean implements Serializable {
         FacesMessage message;
         message = new FacesMessage(FacesMessage.SEVERITY_INFO, estado, mensaje);
         RequestContext.getCurrentInstance().showMessageInDialog(message);
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipoId() {
+        return tipoId;
+    }
+
+    public void setTipoId(String tipoId) {
+        this.tipoId = tipoId;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Eps getEps() {
+        return eps;
+    }
+
+    public void setEps(Eps eps) {
+        this.eps = eps;
+    }
+    
+    public List<String> epsRegistradas() throws ExcepcionServiciosPacientes{
+        for(Eps epss:servicepacientes.obtenerEPSsRegistradas()){
+            nombreEps.add(epss.getNombre());
+        }
+        
+        return nombreEps;
     }
 
 
